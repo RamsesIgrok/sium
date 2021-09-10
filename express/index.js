39188953eefb4d867b81d4b2d6e2516be13f7b45
RamsesIgrok <villella.giuseppe03@gmail.com>
2021-09-10 19:03:30 +0200

    Aggiunte pagine illimitate

--- express/index.js
+++ express/index.js
@@ -2,20 +2,25 @@ require('dotenv').config()
 const cors = require('cors');
 const express = require('express');
 const axios = require('axios');
+const _ = require('lodash');
 const app = express();
 const port = 5000;
 
 app.use(cors());
 app.get('/', async (req, res) => {
-  await axios.get('https://shoppy.gg/api/v1/products', {
+  const finalRes = [];
+
+  for (let i = 1; i > 0; i++) {
+    const response = await axios.get(`https://shoppy.gg/api/v1/products?page=${i}`, {
       headers: {
         'Authorization': process.env.API_KEY,
       }
-  }).then(response => {
-    res.send(response.data);
-  }).catch(err => {
-    console.log(err);
-  });
+    });
+    if (response.data.length == 0) break;
+    finalRes.push(response.data);
+  }
+
+  res.send(_.flatten(finalRes));
 });
 
 app.listen(port, () => {
